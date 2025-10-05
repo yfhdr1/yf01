691d308f3028d8dd698f1a4a913a3763a97c59ac
yfhdr1 <yousif6889@gmail.com>
2025-10-06 00:23:15 +0300

    Create Program.cs
    c#
    
    Signed-off-by: yfhdr1 <yousif6889@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace yf01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GitHub from C#!");
+        }
+    }
+}
